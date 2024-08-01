01e5ecbe13da8d8ac431413936c10be4880ce441
Pravin Dharam <dharpr-cont@mahindra.com>
2024-08-01 13:02:47 +0530

    #initial commit.

--- app/src/androidTest/java/com/cheezycode/quotesviewmodel/MainActivityTest.kt
+++ app/src/androidTest/java/com/cheezycode/quotesviewmodel/MainActivityTest.kt
@@ -1,7 +1,7 @@
 package com.cheezycode.quotesviewmodel
 
 import android.content.Intent
-import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
@@ -10,19 +10,17 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
-import org.hamcrest.CoreMatchers.allOf
-import org.junit.Assert.*
+import org.hamcrest.core.AllOf.allOf
 import org.junit.Rule
 import org.junit.Test
+
 
 class MainActivityTest{
 
     @get:Rule
     val activityScenarioRule = activityScenarioRule<MainActivity>()
-
     @Test
     fun testNextButton_expectedCorrectQuote(){
-
         onView(withId(R.id.btnNext)).perform(click())
         onView(withId(R.id.btnNext)).perform(click())
         onView(withId(R.id.btnNext)).perform(click())
@@ -37,20 +35,4 @@ class MainActivityTest{
         intended(expected)
         Intents.release()
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ app/src/main/java/com/cheezycode/quotesviewmodel/usecase2/FirstActivity.kt
@@ -0,0 +1,15 @@
+package com.cheezycode.quotesviewmodel.usecase2
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import com.cheezycode.quotesviewmodel.R
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_first)
+
+    }
+}
